fd3f4593fef9dce279f015973c32321c72d687c3
unknown <pierresiendo745@gmail.com>
2026-02-02 16:31:37 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_000000128411_PierreESMoningka/week01/GameStore.kt
+++ src/oop_000000128411_PierreESMoningka/week01/GameStore.kt
@@ -4,6 +4,15 @@ fun main() {
     val gameTitle = "Call of Duty"
     val price = 899000
 
+    val finalPrice = printReceipt(originalPrice = price)
+
     println("Game Title: $gameTitle")
     println("Normal Price: $price")
+    println("Final Price: $finalPrice")
+}
+
+fun printReceipt(originalPrice: Int): Double = if (originalPrice > 500000) {
+    originalPrice * 0.8
+} else {
+    originalPrice * 0.9
 }
