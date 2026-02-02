dfecb7e985bff50f6dda7cbf5c2b11065b4cbe65
unknown <pierresiendo745@gmail.com>
2026-02-02 16:28:55 +0700

    week01: (tugas 2) implement discount logic

---
+++ src/oop_000000128411_PierreESMoningka/week01/GameStore.kt
@@ -0,0 +1,9 @@
+package oop_000000128411_PierreESMoningka.week01
+
+fun main() {
+    val gameTitle = "Call of Duty"
+    val price = 899000
+
+    println("Game Title: $gameTitle")
+    println("Normal Price: $price")
+}
