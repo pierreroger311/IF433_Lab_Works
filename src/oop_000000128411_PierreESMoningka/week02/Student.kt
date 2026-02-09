0f19d613fffb8d16f9fd0115ac6a3f7501b59c06
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-02-09 16:03:19 +0700

    week02: add warning valdiation in init block

--- src/oop_000000128411_PierreESMoningka/week02/Student.kt
+++ src/oop_000000128411_PierreESMoningka/week02/Student.kt
@@ -1,4 +1,13 @@
 package oop_000000128411_PierreESMoningka.week02
 
-class Student {
+class Student (val name: String, val nim: String, var major: String) { new *
+    init { new *
+        // Validasi Sederhana: Cek panjang NIM
+        if (nim.length !=5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
 }
