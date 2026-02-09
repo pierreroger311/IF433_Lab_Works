5ec3c02fb6e8ea48967612da41cc9f81a88745ce
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-02-09 16:11:28 +0700

    week02: implement user input and pre-validation

--- src/oop_000000128411_PierreESMoningka/week02/Main.kt
+++ src/oop_000000128411_PierreESMoningka/week02/Main.kt
@@ -1,2 +1,30 @@
 package oop_000000128411_PierreESMoningka.week02
 
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+
+    scanner.close() // Bersihkan buffer newLine (Penyakit klasik Scanner!)
+
+    // Validasi di sisi pemanggil (Main)
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+        // Program berhenti disini untuk mahasiswa ini, tidak membuat objek
+    } else {
+        print("Masukkan Jurusan: ")
+        val major = scanner.next()
+
+        // Instansiasi Objek karena data sudah aman
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai.")
+    }
+}
