7e48ca48fc47c46e181dd8024156b87ecfa827ec
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-02-09 16:36:39 +0700

    week02: implement selection logic for constructors

--- src/oop_000000128411_PierreESMoningka/week02/Main.kt
+++ src/oop_000000128411_PierreESMoningka/week02/Main.kt
@@ -13,8 +13,6 @@ fun main() {
     print("Masukkan NIM (Wajib 5 Karakter): ")
     val nim = scanner.next()
 
-    scanner.close() // Bersihkan buffer newLine (Penyakit klasik Scanner!)
-
     // Validasi di sisi pemanggil (Main)
     if (nim.length != 5) {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
@@ -27,4 +25,22 @@ fun main() {
         val s1 = Student(name, nim, major)
         println("Status: Pendaftaran Selesai.")
     }
+
+    print("Pilih Jalur (1. Reguler, 2. Umum): ")
+    val type = scanner.nextInt()
+    scanner.nextLine()
+
+    if (type == 1) {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+        val s1 = Student(name, nim, major)
+        println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+    } else if (type == 2) {
+        val s2 = Student(name, nim)
+        println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+    } else {
+        println("Pilihan ngawur, pendaftaran batal!")
+    }
+
+    scanner.close() // Bersihkan buffer newLine (Penyakit klasik Scanner!)
 }
