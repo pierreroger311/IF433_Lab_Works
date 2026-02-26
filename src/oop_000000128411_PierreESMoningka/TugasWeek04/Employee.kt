8c8074c365aed1841f7e4c34d9092d011e33953a
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-02-26 14:59:00 +0700

    week04: (task 2) create parent class Employee

---
+++ src/oop_000000128411_PierreESMoningka/TugasWeek04/Employee.kt
@@ -0,0 +1,15 @@
+package oop_000000128411_PierreESMoningka.TugasWeek04
+
+open class Employee(
+    val name: String,
+    val baseSalary: Int
+) {
+
+    open fun work() {
+        println("$name sedang bekerja.")
+    }
+
+    open fun calculateBonus(): Int {
+        return (baseSalary * 10) / 100
+    }
+}
