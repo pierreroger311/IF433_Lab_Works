049dccebd5e6a92297ee174102c96d4067cd83ed
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-02-26 15:00:10 +0700

    week04: (task 2) create Manager class overriding Employee

---
+++ src/oop_000000128411_PierreESMoningka/TugasWeek04/Manager.kt
@@ -0,0 +1,15 @@
+package oop_000000128411_PierreESMoningka.TugasWeek04
+
+class Manager(
+    name: String,
+    baseSalary: Int
+) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang memimpin rapat divisi.")
+    }
+
+    override fun calculateBonus(): Int {
+        return super.calculateBonus() + 500_000
+    }
+}
