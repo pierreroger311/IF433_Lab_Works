6f5dae914a931c416b9b76a01e24b0028e4721e1
unknown <pierresiendo745@gmail.com>
2026-03-02 15:31:19 +0700

    week04: (task 2) create EWallet class

---
+++ src/oop_000000128411_PierreESMoningka/TugasWeek05Task02/EWallet.kt
@@ -0,0 +1,18 @@
+package oop_000000128411_PierreESMoningka.TugasWeek05Task02
+
+class EWallet(accountName: String, var balance: Double) : PaymentMethod(accountName) {
+
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+            println("[$accountName - E-Wallet] Pembayaran Rp$amount berhasil. Sisa saldo: Rp$balance")
+        } else {
+            println("[$accountName - E-Wallet] Saldo tidak cukup untuk transaksi Rp$amount.")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("[$accountName - E-Wallet] Berhasil Top Up Rp$amount. Saldo sekarang: Rp$balance")
+    }
+}
