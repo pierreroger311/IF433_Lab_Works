7bab4b56dc03e838b7f7d7af2f5deaa2d45ea930
unknown <pierresiendo745@gmail.com>
2026-03-02 15:28:33 +0700

    week04: (task 1) test MathHelper overloading in Main

---
+++ src/oop_000000128411_PierreESMoningka/TugasWeek05Task1/Main.kt
@@ -0,0 +1,11 @@
+package oop_000000128411_PierreESMoningka.TugasWeek05Task1
+
+fun main() {
+    val helper = MathHelper()
+
+    helper.hitungLuas(5)
+
+    helper.hitungLuas(10, 5)
+
+    helper.hitungLuas(7.0)
+}
