30b6e9763e2d86820a1c10427743635c1616e83e
unknown <pierresiendo745@gmail.com>
2026-03-02 15:34:00 +0700

    week04: (task 2) implement payment polymorphism

--- src/oop_000000128411_PierreESMoningka/TugasWeek05Task02/Main.kt
+++ src/oop_000000128411_PierreESMoningka/TugasWeek05Task02/Main.kt
@@ -1,4 +1,21 @@
 package oop_000000128411_PierreESMoningka.TugasWeek05Task02
 
-class Main {
+fun main() {
+    // Checkpoint 4: Inisialisasi data
+    val dompetBudi = EWallet("Budi", 50000.0)
+    val kartuBudi = CreditCard("Budi", 100000.0)
+
+    // Masukkan ke dalam list bertipe PaymentMethod (Polymorphism)
+    val daftarPembayaran: List<PaymentMethod> = listOf(dompetBudi, kartuBudi)
+
+    val tagihan = 75000.0
+
+    for (metode in daftarPembayaran) {
+        println("--- Mencoba pembayaran dengan ${metode.accountName} ---")
+
+        // Panggilan awal (E-Wallet akan gagal di sini karena saldo 50rb < tagihan 75rb)
+        metode.processPayment(tagihan)
+
+        println("----------------------------------------------")
+    }
 }
