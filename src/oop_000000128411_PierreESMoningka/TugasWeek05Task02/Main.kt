e7443986cc62cad0ee0ea11d21d885b226dc1f6e
unknown <pierresiendo745@gmail.com>
2026-03-02 15:35:04 +0700

    week04: (task 2) add smart casting for EWallet topUp recovery

--- src/oop_000000128411_PierreESMoningka/TugasWeek05Task02/Main.kt
+++ src/oop_000000128411_PierreESMoningka/TugasWeek05Task02/Main.kt
@@ -1,11 +1,9 @@
 package oop_000000128411_PierreESMoningka.TugasWeek05Task02
 
 fun main() {
-    // Checkpoint 4: Inisialisasi data
     val dompetBudi = EWallet("Budi", 50000.0)
     val kartuBudi = CreditCard("Budi", 100000.0)
 
-    // Masukkan ke dalam list bertipe PaymentMethod (Polymorphism)
     val daftarPembayaran: List<PaymentMethod> = listOf(dompetBudi, kartuBudi)
 
     val tagihan = 75000.0
@@ -13,8 +11,16 @@ fun main() {
     for (metode in daftarPembayaran) {
         println("--- Mencoba pembayaran dengan ${metode.accountName} ---")
 
-        // Panggilan awal (E-Wallet akan gagal di sini karena saldo 50rb < tagihan 75rb)
         metode.processPayment(tagihan)
+
+        if (metode is EWallet) {
+            println("Info: Mendeteksi E-Wallet. Melakukan Auto Top-Up...")
+
+            metode.topUp(50000.0)
+
+            println("Mencoba ulang pembayaran...")
+            metode.processPayment(tagihan)
+        }
 
         println("----------------------------------------------")
     }
