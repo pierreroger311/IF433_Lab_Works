9e859fed76d026e89b6341ab101df0621c0cf04e
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-03-09 17:39:31 +0700

    week06: (task) intantiate smart devices in main

---
+++ src/oop_000000128411_PierreESMoningka/TugasWeek06/Main.kt
@@ -0,0 +1,7 @@
+package oop_000000128411_PierreESMoningka.TugasWeek06
+
+fun main() {
+    // Instansiasi Perangkat
+    val lampuTamu = SmartLamp("L01", "Ruang Tamu")
+    val googleNest = SmartSpeaker("S01", "Google Nest Dapur")
+    val ezvizGarasi = SmartCCTV("C01", "Ezviz Garasi")
