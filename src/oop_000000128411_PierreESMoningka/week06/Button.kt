b0a24d082ce2e8311cb6c3fc6d85ed4cf9e69e18
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-03-09 17:02:53 +0700

    week06: create Button class implementing Clickable

--- src/oop_000000128411_PierreESMoningka/week06/Button.kt
+++ src/oop_000000128411_PierreESMoningka/week06/Button.kt
@@ -2,6 +2,6 @@ package oop_000000128411_PierreESMoningka.week06
 
 class Button(override val name: String) : Clickable {
     override fun click() {
-        println("Tombol '$name' berhasil diklik!)
+        println("Tombol '$name' berhasil diklik!")
     }
 }
