dbbcd3ce9742e545d2e023c800160152e9e708b7
unknown <pierresiendo745@gmail.com>
2026-03-16 14:56:28 +0700

    week07: implement data class for structural equality

--- src/oop_000000128411_PierreESMoningka/week0702/UserModels.kt
+++ src/oop_000000128411_PierreESMoningka/week0702/UserModels.kt
@@ -2,3 +2,5 @@ package oop_000000128411_PierreESMoningka.week0702
 
 class RegularUser(val name: String, val age: Int) {
 }
+
+data class DataUser(val name: String, val age: Int)
--- src/oop_000000128411_PierreESMoningka/week0702/main.kt
+++ src/oop_000000128411_PierreESMoningka/week0702/main.kt
@@ -2,8 +2,8 @@ package oop_000000128411_PierreESMoningka.week0702
 
 fun main() {
     println("\n=== TEST REGULAR CLASS ===")
-    val reg1 = RegularUser("Alice", 22)
-    val reg2 = RegularUser("Alice", 22)
-    println(reg1) // Akan mencetak memori hash
-    println("Sama? ${reg1 == reg2}") // False
+    val data1 = DataUser("Alice", 22)
+    val data2 = DataUser("Alice", 22)
+    println(data1) // Otomatis readable format
+    println("Sama? ${data1 == data2}") // True
 }
