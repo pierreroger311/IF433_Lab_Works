42c69cc4fb487fcaa99c4052106010b17343afc1
unknown <pierresiendo745@gmail.com>
2026-03-16 15:07:54 +0700

    week07: resolve sealed class exhaustive check

--- src/oop_000000128411_PierreESMoningka/week0703/Main.kt
+++ src/oop_000000128411_PierreESMoningka/week0703/Main.kt
@@ -8,5 +8,6 @@ fun main() {
     val uiMessage = when (response) {
         is ApiResponse.Success -> "Tampilkan: ${response.data}"
         is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        ApiResponse.Loading -> "Tampilkan Spinner"
     }
 }
