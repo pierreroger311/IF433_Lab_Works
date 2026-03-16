9a39965bd149d50cf8433debabfbf2151efabc3b
unknown <pierresiendo745@gmail.com>
2026-03-16 15:13:57 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_000000128411_PierreESMoningka/TugasWeek07/Main.kt
+++ src/oop_000000128411_PierreESMoningka/TugasWeek07/Main.kt
@@ -4,4 +4,24 @@ fun main() {
     // Simulasi Singleton
     GameManager.startGame()
     GameManager.startGame()
+
+    println("\n--- Simulasi Rarity & Factory ---")
+    // Drop chance Legendary
+    println("Drop chance item LEGENDARY adalah: ${ItemRarity.LEGENDARY.dropChance}%")
+
+    // Factory pattern
+    val starterWeapon = Weapon.forgeStarterSword()
+    println("Senjata awal: ${starterWeapon.item.name} dengan durability ${starterWeapon.durability}")
+
+    // CHECKPOINT 19: Commit "week07: (task) simulate rarity and factory instantiation"
+
+    println("\n--- Simulasi Event & Immutability ---")
+    // Upgrade dengan copy()
+    val upgradedItem = starterWeapon.item.copy(name = "Pedang Kayu Upgrade", damage = 25)
+
+    // Simulasi event berurutan
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+    processEvent(BattleState.LootDropped(upgradedItem))
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
 }
