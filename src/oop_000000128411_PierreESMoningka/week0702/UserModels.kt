89b5dcb0bae07df744435be784ddd53ccaa008cb
unknown <pierresiendo745@gmail.com>
2026-03-16 14:53:53 +0700

    week07: (trial) demonstrate regular class equality failure

---
+++ src/oop_000000128411_PierreESMoningka/week0702/UserModels.kt
@@ -0,0 +1,4 @@
+package oop_000000128411_PierreESMoningka.week0702
+
+class RegularUser(val name: String, val age: Int) {
+}
---
+++ src/oop_000000128411_PierreESMoningka/week0702/main.kt
@@ -0,0 +1,9 @@
+package oop_000000128411_PierreESMoningka.week0702
+
+fun main() {
+    println("\n=== TEST REGULAR CLASS ===")
+    val reg1 = RegularUser("Alice", 22)
+    val reg2 = RegularUser("Alice", 22)
+    println(reg1) // Akan mencetak memori hash
+    println("Sama? ${reg1 == reg2}") // False
+}
