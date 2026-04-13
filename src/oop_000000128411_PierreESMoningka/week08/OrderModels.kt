b9e6e5602db85bc7bee0b6df6d30d5768cfc5349
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-04-13 13:29:36 +0700

    week08: create nested classes for Order

---
+++ src/oop_000000128411_PierreESMoningka/week08/OrderModels.kt
@@ -0,0 +1,6 @@
+package oop_000000128411_PierreESMoningka.week08
+
+class City(val name: String)
+class Address(val city: City?)
+class ProductDetails(val address: Address?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
