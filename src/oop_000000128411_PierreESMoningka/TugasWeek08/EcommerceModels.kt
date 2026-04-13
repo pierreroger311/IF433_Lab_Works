717bbf408fb4206b14b3dbc1330fb9b00b934746
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-04-13 14:04:53 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_000000128411_PierreESMoningka/TugasWeek08/EcommerceModels.kt
@@ -0,0 +1,6 @@
+package oop_000000128411_PierreESMoningka.TugasWeek08
+
+sealed class Product {
+    data class Electronic(val id: String, val name: String, val warrantyMonths: Int) : Product()
+    data class Clothing(val id: String, val name: String, val size: String) : Product()
+}
