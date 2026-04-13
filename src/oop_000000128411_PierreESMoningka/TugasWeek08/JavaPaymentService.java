58137696b7e2653ea7a17ac0f1efef8c15fbc07a
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-04-13 14:06:26 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_000000128411_PierreESMoningka/TugasWeek08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_000000128411_PierreESMoningka.TugasWeek08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
