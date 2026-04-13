acc3bfce4ac2f3e089a4aeac17cbd7840abd67c8
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-04-13 13:53:19 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_000000128411_PierreESMoningka/week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_000000128411_PierreESMoningka.week08;
+
+public class LegacyJavaAPI {
+    // Fungsi ini ditulis dalam Java. Kotlin tidak tahu apakah kembaliannya Nullable
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE200_OK";
+    }
+}
