a45c8eb69733bea97d64a32987bc78eb73c0f43a
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-04-13 13:35:21 +0700

    week08: implement safe call with let block

--- src/oop_000000128411_PierreESMoningka/week08/Main.kt
+++ src/oop_000000128411_PierreESMoningka/week08/Main.kt
@@ -7,4 +7,15 @@ fun Main() {
     // Rantai Safe Calls yang elegan
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
     println("Tujuan pengiriman: $destination")
+
+    println("\n=== TEST LET BLOCK ===")
+    val validOrder = Order(null, 250000)
+
+    val recepit = validOrder.totalPrice?.let { price ->
+        // Blok ini HANYA jalan jika totalPrice tidak null
+        val tax = price * 0.11
+        "Transaksi valid. Harga: Rp$price, Pajak: Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di-set!"
+
+    println(recepit)
 }
