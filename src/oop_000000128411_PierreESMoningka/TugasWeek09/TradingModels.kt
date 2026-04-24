8671ba85f3be2a6b9b360da8ad5991bcdd17d1fa
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-04-24 22:53:38 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_000000128411_PierreESMoningka/TugasWeek09/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_000000128411_PierreESMoningka.TugasWeek09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
