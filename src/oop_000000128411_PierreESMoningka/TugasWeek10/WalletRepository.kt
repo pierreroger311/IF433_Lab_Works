b7b2d4a7d8d5674c55d59ae85b37915d03a0a6bb
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-04-27 11:46:16 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_000000128411_PierreESMoningka/TugasWeek10/WalletRepository.kt
@@ -0,0 +1,13 @@
+package oop_000000128411_PierreESMoningka.TugasWeek10
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
