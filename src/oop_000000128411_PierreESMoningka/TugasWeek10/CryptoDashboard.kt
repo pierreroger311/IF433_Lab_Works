bf94e35acf4bda851af4c85e5b95835a8bbf9d89
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-04-27 11:51:06 +0700

    week10: (task) print dashboard using forEach on generic list

--- src/oop_000000128411_PierreESMoningka/TugasWeek10/CryptoDashboard.kt
+++ src/oop_000000128411_PierreESMoningka/TugasWeek10/CryptoDashboard.kt
@@ -8,4 +8,9 @@ fun main() {
     coinRepo.add(Coin("USDT", 1500.0))
 
     val response = ApiResponse("200 OK", coinRepo.getAll())
+    println("Response Status: ${response.status}")
+    println("--- Coin List ---")
+    response.data.forEach { coin ->
+        println("Coin: ${coin.name} | Balance: ${coin.balance}")
+    }
 }
