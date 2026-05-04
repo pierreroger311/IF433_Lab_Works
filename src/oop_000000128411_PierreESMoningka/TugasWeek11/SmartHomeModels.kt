88def45375b2f996f829f17429161516183e7538
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-05-04 13:16:59 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_000000128411_PierreESMoningka/TugasWeek11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_000000128411_PierreESMoningka.TugasWeek11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
