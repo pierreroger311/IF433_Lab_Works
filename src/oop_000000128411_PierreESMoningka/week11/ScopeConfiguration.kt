aa6c956d3a11bf8c26611cea355d62d6d00c6f25
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-05-04 13:11:57 +0700

    week11: implement also for logging side effects

--- src/oop_000000128411_PierreESMoningka/week11/ScopeConfiguration.kt
+++ src/oop_000000128411_PierreESMoningka/week11/ScopeConfiguration.kt
@@ -1,3 +1,19 @@
 package oop_000000128411_PierreESMoningka.week11
 
 data class User(var name: String = "", var age: Int = 0)
+
+fun main() {
+    println("=== TEST APPLY ===")
+    val user = User().apply {
+        name = "Alex"
+        age = 25
+    }
+    println(user)
+
+    println("\n=== TEST ALSO ===")
+    val numbers  = mutableListOf(1, 2, 3)
+    numbers.also {
+        println("Log Sebelum ditambah: $it")
+    }.add(4)
+    println("Setelah ditambah: $numbers")
+}
