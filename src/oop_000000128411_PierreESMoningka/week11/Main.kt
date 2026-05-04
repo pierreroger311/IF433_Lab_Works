5e0aaf1cbd95a1e510a91e79c7deaf1ace95a1c1
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-05-04 12:59:01 +0700

    week11: test extension functions in main

---
+++ src/oop_000000128411_PierreESMoningka/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_000000128411_PierreESMoningka.week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text:String? = null
+    println("Apakah null/empty? ${text.isNUllOrEmptyCustom()}")
+}
