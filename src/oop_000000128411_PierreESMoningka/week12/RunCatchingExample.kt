1e476fe0c071347679fa46359755c50413778e31
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-05-11 14:46:58 +0700

    week12: implement basic runCatching

---
+++ src/oop_000000128411_PierreESMoningka/week12/RunCatchingExample.kt
@@ -0,0 +1,8 @@
+package oop_000000128411_PierreESMoningka.week12
+
+fun main() {
+    println("=== TEST RUNCATCHING ===")
+    val result: Result<Int> = runCatching {
+        "42X".toInt()
+    }
+}
