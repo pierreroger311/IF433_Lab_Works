77ed19b40873d4e4cf4af3248b9671ebe0ab2caf
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-05-11 14:55:45 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_000000128411_PierreESMoningka/TugasWeek12/SmartFeeder.kt
+++ src/oop_000000128411_PierreESMoningka/TugasWeek12/SmartFeeder.kt
@@ -3,5 +3,9 @@ package oop_000000128411_PierreESMoningka.TugasWeek12
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
 
-    return availableGram
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+
+    return availableGram // Placeholder
 }
