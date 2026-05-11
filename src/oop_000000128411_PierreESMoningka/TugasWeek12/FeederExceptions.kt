4f29bffdd4107fb98f6cfce75bcda542e8ecf11d
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-05-11 14:54:34 +0700

    week12: (task) implement dispenseKibble function

--- src/oop_000000128411_PierreESMoningka/TugasWeek12/FeederExceptions.kt
+++ src/oop_000000128411_PierreESMoningka/TugasWeek12/FeederExceptions.kt
@@ -1,3 +1,9 @@
 package oop_000000128411_PierreESMoningka.TugasWeek12
 
 sealed class FeederException(msg: String) : Exception(msg)
+
+class FoodEmptyException(requested: Int, available: Int) :
+    FeederException("Kibble tidak cukup! Diminta $requested gr, sisa $available gr")
+
+class DispenserJamException :
+    FeederException("Wadah dispenser tersangkut/macet!")
