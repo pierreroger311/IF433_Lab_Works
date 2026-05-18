266b4005d8930d43f792fa45ad7470f22132c89d
pierreroger311 <51148513+pierreroger311@users.noreply.github.com>
2026-05-18 14:27:26 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_000000128411_PierreESMoningka/TugasWeek13/CryptoTradeLogger.kt
@@ -0,0 +1,9 @@
+package oop_000000128411_PierreESMoningka.TugasWeek13
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
